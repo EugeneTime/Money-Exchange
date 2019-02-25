5c717192c87f95568b9653c35d080619d73c421f
EugeneTime <eugenetime@bk.ru>
2019-02-25 22:31:38 +0300

    perf: implement method with using objects

--- src/index.js
+++ src/index.js
@@ -1,31 +1,14 @@
 module.exports = function makeExchange(currency) {
-
-    let sumCoins = currency;
-    let h, q, d, n, p;
-    let typeCoins = {};
-
-    if (currency > 10000) return{error: "You are rich, my friend! We don't have so much coins for exchange"};
-    if (currency <= 0) return{};
-    
-    while (sumCoins >= 1) {
-        switch (true) {
-            case (sumCoins >= 50):
-                h = Math.floor(sumCoins / 50); sumCoins %= 50; if(h !== 0) typeCoins.H = h;
-                break;
-            case (sumCoins >= 25):
-                q = Math.floor(sumCoins / 25); sumCoins %= 25; if(q !== 0) typeCoins.Q = q;
-                break;
-            case (sumCoins >= 10):
-                d = Math.floor(sumCoins / 10); sumCoins %= 10; if(d !== 0) typeCoins.D = d;
-                break;
-            case (sumCoins >= 5):
-                n = Math.floor(sumCoins / 5); sumCoins %= 5; if(n !== 0) typeCoins.N = n;
-                break;
-            case (sumCoins >= 1):
-                p = Math.floor(sumCoins / 1); sumCoins = 0; if(p !== 0) typeCoins.P = p;
-                break;
-        }
-    }
-
-    return typeCoins;
+    if (currency > 10000) return { error: "You are rich, my friend! We don't have so much coins for exchange" }; 
+    let coinsType = {}; 
+    if (currency <= 0) return coinsType; 
+    let coinsPrice = { 'H': 50, 'Q': 25, 'D': 10, 'N': 5, 'P': 1 }; 
+    for (coin in coinsPrice) { 
+      let coinsCount = Math.floor(currency / coinsPrice[coin]); 
+      if (coinsCount) { 
+        currency = Math.floor(currency % coinsPrice[coin]);
+        coinsType[coin] = coinsCount; 
+      } 
+    } 
+    return coinsType; 
 }
